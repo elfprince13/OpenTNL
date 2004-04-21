1567ec1931ed1384c651741fe6d7f9c5186cce52
s_alanet <s_alanet>
2004-04-21 01:39:32 +0000

    Added connection status display.

--- zap/UIGame.cpp
+++ zap/UIGame.cpp
@@ -94,6 +94,30 @@ void GameUserInterface::idle(U32 timeDelta)
    }
 }
 
+static char * connectStates[] = {
+      //NotConnected,              ///< Initial state of a NetConnection instance - not connected.
+      "Not connected...",
+      //AwaitingChallengeResponse, ///< We've sent a challenge request, awaiting the response.
+      "Sending challenge request.",
+      //SendingPunchPackets,       ///< The state of a pending arranged connection when both sides haven't heard from the other yet
+      "Punching through firewalls.",
+      //ComputingPuzzleSolution,   ///< We've received a challenge response, and are in the process of computing a solution to its puzzle.
+      "Computing puzzle solution.",
+      //AwaitingConnectResponse,   ///< We've received a challenge response and sent a connect request.
+      "Sent connect request.",
+      //ConnectTimedOut,           ///< The connection timed out during the connection process.
+      "Connection timed out.",
+      //ConnectRejected,           ///< The connection was rejected.
+      "Connection rejected.",
+      //Connected,                 ///< We've accepted a connect request, or we've received a connect response accept.
+      "Connected.",
+      //Disconnected,              ///< The connection has been disconnected.
+      "Disconnected.",
+      //TimedOut,                  ///< The connection timed out.
+      "Connection timed out.",
+      ""
+};
+
 void GameUserInterface::render()
 {
    glMatrixMode(GL_MODELVIEW);
@@ -108,7 +132,10 @@ void GameUserInterface::render()
    {
       glColor3f(1,1,1);
       drawCenteredString(290, 30, "Connecting to server...");
-      drawCenteredString(330, 20, "Press <ESC> to abort");
+
+      if(gClientGame->getConnectionToServer())
+         drawCenteredString(330, 16, connectStates[gClientGame->getConnectionToServer()->getConnectionState()]);
+      drawCenteredString(370, 20, "Press <ESC> to abort");
    }
 
    glTranslatef(400, 300, 0);
